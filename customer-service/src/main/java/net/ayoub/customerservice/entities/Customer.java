3e67b83c973b7b4a4d66d2c831aae5b051457c7c
ayoub chaib <watchpro2@gmail.com>
2025-11-29 20:32:17 +0100

    Create Customer entity

---
+++ customer-service/src/main/java/net/ayoub/customerservice/entities/Customer.java
@@ -0,0 +1,21 @@
+package net.ayoub.customerservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @ToString @NoArgsConstructor @AllArgsConstructor @Builder
+public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+}
